a76f37f964ba39a6b9aed5014fcc4cec3e62f4a4
Martin Dilger <martin@effectivetrainings.de>
2024-03-05 20:59:09 +0100

    draft

--- foo2/src/main/kotlin/de/nebulit/testy/EventModelingApplication.kt
+++ foo2/src/main/kotlin/de/nebulit/testy/EventModelingApplication.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableJpaRepositories
