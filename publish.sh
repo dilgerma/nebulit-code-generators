1f2ba0259d8a9d1037d6acf90b6b1f8d3d473a88
Martin Dilger <martin@effectivetrainings.de>
2024-04-01 14:36:42 +0200

    split build / publish

--- publish.sh
+++ publish.sh
@@ -2,3 +2,4 @@
 npm version patch
 npm publish
 docker build -t ghcr.io/dilgerma/nebulit-spring-boot-generator --secret id=npmrc,src=$HOME/.npmrc .
+docker push ghcr.io/dilgerma/nebulit-spring-boot-generator
